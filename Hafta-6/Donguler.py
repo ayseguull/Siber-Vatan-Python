c8d90ce1fc209cf315874e89093c0039099d611d
Aysegul <158616822+ayseguull@users.noreply.github.com>
2024-02-07 18:49:59 +0300

    Create Donguler.py

---
+++ Hafta-6/Donguler.py
@@ -0,0 +1,69 @@
+meyveler= ["elma","kivi","limon","bogurtlen"]
+
+print(meyveler)
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+sayilar=[10,20,30,40,50]
+
+sayilar[0]+=sayilar[1]
+sayilar[0]+=sayilar[1]
+sayilar[0]+=sayilar[3]
+
+print(sayilar[0])
+
+toplam=0
+
+for sayi in sayilar:
+    toplam+=sayi
+
+print(toplam)
+
+""""""""""""
+
+meyveler= ["elma","kivi","limon","bogurtlen"]
+
+
+for meyve in meyveler:
+    print(meyve)
+
+
+""
+sayilar=[2,3,5,7,0,3,53,4,65,34]
+ 
+ teksayilar = []
+ ciftsayilar = []
+
+ for sayi in sayilar :
+ if sayi %2 != 0:
+   teksayilar.append(sayi)
+
+ else: ciftsayilar.append(sayi)
+
+ print("tek sayilar:",teksayilar)
+
+""""""""
+
+i = 1
+while i<= 10
+print(i)
+i += 1
+
+""""""
+
+sayi = int(input("bir sayı griiniz: "))
+i = 1
+while i <= sayi:
+   print(i)
+   i += 1
+ 
+ ""
+
+sayilar = [0]
+
+for i in range(5):
+   sayi = int(input( f"7{i+1} sayıyı girin: "))
+   sayilar.append(sayi)
+
+print("girilen sayılar:",sayilar)
+
